6d04ae66cba65eb4a4472a3f274cb5dc659f8f7e
Aaron Boyer <aboyer@roomandboard.com>
2020-05-23 16:07:57 -0500

    Converted spaces to tabs.

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,63 @@
 import Head from 'next/head'
 
 const Home = () => (
-  <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+	<div className="container">
+		<Head>
+			<title>Create Next App - Dev</title>
+			<link rel="icon" href="/favicon.ico"/>
+		</Head>
 
-    <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+		<main>
+			<h1 className="title">
+				Welcome to <a href="https://nextjs.org">Next.js!</a>
+			</h1>
 
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
+			<p className="description">
+				Get started by editing <code>pages/index.js</code>
+			</p>
 
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+			<div className="grid">
+				<a href="https://nextjs.org/docs" className="card">
+					<h3>Documentation &rarr;</h3>
+					<p>Find in-depth information about Next.js features and API.</p>
+				</a>
 
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
+				<a href="https://nextjs.org/learn" className="card">
+					<h3>Learn &rarr;</h3>
+					<p>Learn about Next.js in an interactive course with quizzes!</p>
+				</a>
 
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
+				<a
+					href="https://github.com/zeit/next.js/tree/master/examples"
+					className="card"
+				>
+					<h3>Examples &rarr;</h3>
+					<p>Discover and deploy boilerplate example Next.js projects.</p>
+				</a>
 
-        <a
-          href="https://vercel.com/new?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+				<a
+					href="https://vercel.com/new?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+					className="card"
+				>
+					<h3>Deploy &rarr;</h3>
+					<p>
+						Instantly deploy your Next.js site to a public URL with Vercel.
+					</p>
+				</a>
+			</div>
+		</main>
 
-    <footer>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/vercel.svg" alt="Vercel Logo" />
-      </a>
-    </footer>
+		<footer>
+			<a
+				href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				Powered by <img src="/vercel.svg" alt="Vercel Logo"/>
+			</a>
+		</footer>
 
-    <style jsx>{`
+		<style jsx>{`
       .container {
         min-height: 100vh;
         padding: 0 0.5rem;
@@ -184,7 +184,7 @@ const Home = () => (
       }
     `}</style>
 
-    <style jsx global>{`
+		<style jsx global>{`
       html,
       body {
         padding: 0;
@@ -197,7 +197,7 @@ const Home = () => (
         box-sizing: border-box;
       }
     `}</style>
-  </div>
+	</div>
 )
 
 export default Home
