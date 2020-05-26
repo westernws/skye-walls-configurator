1dd41b4884f5da1374c6bf4039eae7bbc15bc18c
Aaron Boyer <aboyer@roomandboard.com>
2020-05-26 18:33:31 -0500

    More breakpoint style tweaks.

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,7 +14,7 @@ export default () => {
 			<main className="Main space-y-20">
 				<section className="flex justify-center mb-10">
 					<div className="w-12/12 md:w-7/12 mt-6">
-						<h1 className="Heading Heading--ruled md:text-5xl">Design Your Door</h1>
+						<h1 className="Heading Heading--ruled text-4xl md:text-5xl">Design Your Door</h1>
 						<p className="max-w-xl">
 							No two transformations are exactly alike. That’s why we designed moving glass walls from
 							Skye Walls to be customizable to your aesthetics and lifestyle.
@@ -22,11 +22,12 @@ export default () => {
 					</div>
 				</section>
 				<section className="space-y-20">
-					<div className="space-y-12">
-						<h2 className="Heading Heading--bordered">
-							<span className="Heading-rule" />
+					<div className="space-y-6 lg:space-y-12">
+						<h2 className="Heading Heading--bordered flex-col text-2xl lg:text-4xl lg:flex-row">
+							<span className="Heading-rule hidden lg:block" />
 							Sliding
-							<span className="Heading-rule" />
+							<span className="Heading-rule hidden lg:block" />
+							<span className="Heading-centerRule block mt-2 lg:hidden" />
 						</h2>
 						<div className="flex justify-center">
 							<div className="w-12/12 md:w-7/12">
