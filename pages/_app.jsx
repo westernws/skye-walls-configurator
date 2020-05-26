b98d303a111b45bca7211143a97da9778e58fef6
Aaron Boyer <aboyer@roomandboard.com>
2020-05-26 16:03:48 -0500

    CSS import change.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import '~/styles/main.css';
+import '#/main.css';
 
 export default function SkyeConfigurator({ Component, pageProps }) {
 	return <Component {...pageProps} />;
