8f6b944b96414933590b03bc9e80d4edfc6bda0b
Aaron Boyer <aboyer@roomandboard.com>
2020-07-04 18:45:51 -0500

    feature: Adding model menu

--- src/Components/MenuListItemProduct.jsx
+++ src/Components/MenuListItemProduct.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import cn from 'classnames';
 
 import { DummyImage } from '~/Components/DummyImage';
+
 
 export const MenuListItemProduct = ({ product, isSelected = false }) => {
 	const { displayName } = product;
 
 	return (
-		<li className="bg-white">
+		<li className={cn('bg-white', {
+			'border-b-4': isSelected,
+			'border-red': isSelected,
+		})}
+		>
 			<button
-				className="p-4 w-full flex items-center"
+				className="p-4 w-full flex items-center justify-between"
 				type="button"
 				onClick={() => {
 					console.log('click');
@@ -18,7 +24,14 @@ export const MenuListItemProduct = ({ product, isSelected = false }) => {
 					<DummyImage className="Menu-itemModelProductImg" width="100" height="88" />
 					<div className="text-sm text-left">{displayName}</div>
 				</div>
-				<div>{isSelected && '!!'}</div>
+				{
+					isSelected &&
+					<div className="Menu-rightCol">
+						<div className="bg-red rounded-full border-white w-5 p-1">
+							<img src="/images/check-solid-white.svg" alt="" />
+						</div>
+					</div>
+				}
 			</button>
 		</li>
 	);
