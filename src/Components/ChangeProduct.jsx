fbddb53fbd20dbece6715f986b2c1a2ca109f199
Aaron Boyer <aboyer@roomandboard.com>
2020-07-06 17:48:12 -0500

    feature: Adding change product modal content.

--- src/Components/ChangeProduct.jsx
+++ src/Components/ChangeProduct.jsx
@@ -5,15 +5,17 @@ import 'mobx-react-lite/batchingForReactDom';
 import { ExchangeAltSolid } from '~/Components/svg/ExchangeAltSolid.svg';
 import { useMst } from '~/Stores/App.store';
 import { useInput } from '~/util/useInput';
+import { ProductSelect } from '~/Components/ProductSelect';
 
 export const ChangeProduct = observer(({ selectedProduct }) => {
 	const appStore = useMst();
-	const { bind } = useInput();
+	const { bind, value } = useInput(selectedProduct.collectionName);
 	const { modal, productCollections } = appStore;
+	const selectedCollection = productCollections.find(productCollection => productCollection.name === value);
 
 	return (
 		<div className="flex flex-col space-y-8 xl:flex-row xl:space-y-0 xl:space-x-4">
-			<div className="xl:w-1/2 xl:p-6 overflow-auto">
+			<div className="xl:w-1/2 xl:p-6 xl:overflow-auto xl:h-screen">
 				<div className="flex flex-col items-center p-12 xl:p-32">
 					<div className="text-gray-light w-12 xl:w-16">
 						<ExchangeAltSolid />
@@ -36,8 +38,16 @@ export const ChangeProduct = observer(({ selectedProduct }) => {
 					</button>
 				</div>
 			</div>
-			<div className="xl:p-6 xl:w-1/2">
-				<select className="Select w-full" defaultValue={selectedProduct.collectionName} {...bind}>
+			<div className="xl:p-6 xl:w-1/2 xl:space-y-2 xl:overflow-auto xl:h-screen">
+				<label htmlFor="select-product" className="hidden text-gray-light text-sm xl:block">
+					Select Product:
+				</label>
+				<select
+					id="select-product"
+					className="Select w-full"
+					defaultValue={selectedProduct.collectionName}
+					onChange={bind.onChange}
+				>
 					{
 						productCollections.map(productGroup => (
 							<option key={productGroup.name} value={productGroup.name}>
@@ -46,6 +56,30 @@ export const ChangeProduct = observer(({ selectedProduct }) => {
 						))
 					}
 				</select>
+				{
+					Boolean(selectedCollection) &&
+					<div>
+						{
+							selectedCollection.hasProductGroup &&
+							selectedCollection.productGroups.map(productGroup => (
+								<ProductSelect
+									key={productGroup.name}
+									displayGroupName={productGroup.displayGroupName}
+									products={productGroup.products}
+									selectedProduct={selectedProduct}
+								/>
+							))
+						}
+						{
+							!selectedCollection.hasProductGroup &&
+							<ProductSelect
+								displayGroupName={selectedCollection.displayGroupName}
+								products={selectedCollection.products}
+								selectedProduct={selectedProduct}
+							/>
+						}
+					</div>
+				}
 			</div>
 		</div>
 	);
---
+++ src/Components/ProductSelect.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import cn from 'classnames';
+import { observer } from 'mobx-react-lite';
+import 'mobx-react-lite/batchingForReactDom';
+
+import { DummyImage } from '~/Components/DummyImage';
+import { useMst } from '~/Stores/App.store';
+
+export const ProductSelect = observer(({ displayGroupName, products, selectedProduct }) => {
+	const { isMediaQueryXl } = useMst();
+
+	return (
+		<>
+			<h2 className="border-b border-gray-light py-2 text-sm font-bold">{displayGroupName}</h2>
+			{
+				products.map((product) => {
+					const isSelectedProduct = product.name === selectedProduct.name;
+
+					return (
+						<div
+							key={product.name}
+							className={cn('flex p-4 space-x-4', {
+								'bg-gray-lighter': isSelectedProduct,
+							})}
+						>
+							<div className="flex-shrink-0">
+								<DummyImage width={isMediaQueryXl ? '165' : '250'} height="80" />
+							</div>
+							<div className="space-y-2 flex-grow flex flex-col justify-center">
+								<h3 className="text-sm uppercase font-bold">{product.displayName}</h3>
+								<button
+									type="button"
+									className="Button Button--secondarySmall w-full"
+									disabled={isSelectedProduct}
+								>
+									{isSelectedProduct ? 'Selected' : 'Select'}
+								</button>
+							</div>
+						</div>
+					);
+				})
+			}
+		</>
+	);
+});
