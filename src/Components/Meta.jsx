ba017305dd02fe6beffbb5423e4298c4195b9dda
Aaron Boyer <aboyer@roomandboard.com>
2021-01-24 15:59:02 -0600

    refactor: Social media meta tags.

--- src/Components/Meta.jsx
+++ src/Components/Meta.jsx
@@ -4,32 +4,22 @@ import Head from 'next/head';
 export const Meta = () => {
 	return (
 		<Head>
-			<title>Skye Walls Configurator &mdash; Western Window Systems</title>
+			<title>Skye Walls Builder &mdash; Western Window Systems</title>
 			<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
-			<meta name="description" content="FPO" />
 
-			<meta name="google" content="nositelinkssearchbox" />
-			<meta property="og:url" content="https://example.com/page.html" />
-			<meta property="og:type" content="website" />
-			<meta property="og:title" content="Content Title" />
-			<meta property="og:image" content="https://example.com/image.jpg" />
-			<meta property="og:image:alt" content="A description of what is in the image (not a caption)" />
-			<meta property="og:description" content="Description Here" />
-			<meta property="og:site_name" content="Site Name" />
-			<meta property="og:locale" content="en_US" />
-			<meta property="article:author" content="" />
-			<meta name="twitter:card" content="summary" />
-			<meta name="twitter:site" content="@site_account" />
-			<meta name="twitter:creator" content="@individual_account" />
-			<meta name="twitter:url" content="https://example.com/page.html" />
-			<meta name="twitter:title" content="Content Title" />
-			<meta name="twitter:description" content="Content description less than 200 characters" />
-			<meta name="twitter:image" content="https://example.com/image.jpg" />
-			<meta
-				name="twitter:image:alt"
-				content="A text description of the image conveying the essential nature of an image to users who are
-					visually impaired. Maximum 420 characters."
-			/>
+			{/* COMMON TAGS */}
+			<meta charset="utf-8" />
+			{/* Search Engine */}
+			<meta name="description" content="Build your own Skye Walls environment" />
+			{/* Schema.org for Google */}
+			<meta itemProp="name" content="Skye Walls Builder" />
+			<meta itemProp="description" content="Build your own Skye Walls environment" />
+			{/* Open Graph general (Facebook, Pinterest & Google+) */}
+			<meta name="og:title" content="Skye Walls Builder" />
+			<meta name="og:description" content="Build your own Skye Walls environment" />
+			<meta name="og:url" content="https://build.skyewallsbywws.com" />
+			<meta name="og:site_name" content="Skye Walls Builder" />
+			<meta name="og:type" content="website" />
 
 			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 			<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
