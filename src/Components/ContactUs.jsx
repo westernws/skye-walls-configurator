f9cdb68a111469584d0668ce5ca2565fc7484647
Aaron Boyer <aboyer@roomandboard.com>
2021-02-06 14:01:06 -0600

    refactor: Removing contact us intro text.

--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -12,14 +12,11 @@ export const ContactUs = () => {
 	} = useMst();
 
 	return (
-		<>
-			<p>Have a question? Send us an email and we’ll get back to you shortly.</p>
-			<iframe
-				src={`https://go.westernwindowsystems.com/l/694473/2021-01-24/4w5vp5?hideComments&productConfig=${selectedOptions}`}
-				title="Contact Us"
-				className="w-full my-8 mx-auto"
-				style={{ minHeight: '300px' }}
-			/>
-		</>
+		<iframe
+			src={`https://go.westernwindowsystems.com/l/694473/2021-01-24/4w5vp5?hideComments&productConfig=${selectedOptions}`}
+			title="Contact Us"
+			className="w-full my-8 mx-auto"
+			style={{ minHeight: '300px' }}
+		/>
 	);
 };
